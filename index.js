39f6bb9696795442f3338c3869cda6544e575bc6
Avi <97816052+avis-scripts@users.noreply.github.com>
2022-05-18 04:47:47 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+var randomNumber1 = Math.round(Math.random() * 5) + 1;
+var randomNumber2 = Math.round(Math.random() * 5) + 1;
+
+document.querySelector("img.img1").setAttribute("src", "./images/dice" + randomNumber1 + ".png");
+document.querySelector("img.img2").setAttribute("src", "./images/dice" + randomNumber2 + ".png");
+
+if (randomNumber1 > randomNumber2) {
+	document.querySelector("h1").innerHTML = "Player 1 wins!!";
+} else if (randomNumber2 > randomNumber1) {
+	document.querySelector("h1").innerHTML = "Player 2 wins!!";
+} else {
+	document.querySelector("h1").innerHTML = "Draw!! 😵 Everybody wins!!";
+}
